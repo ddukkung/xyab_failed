928941377947f7ecddcf4de11f3c57b329890a7f
ddukkung <gjznn3657@gmail.com>
2022-02-24 23:21:45 +0900

    글 목록 뷰페이지 만듬

--- src/main/java/com/miree/xyab/controller/BoardController.java
+++ src/main/java/com/miree/xyab/controller/BoardController.java
@@ -1,7 +1,10 @@
 package com.miree.xyab.controller;
 
 import com.miree.xyab.service.BoardService;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @Controller
 @RequestMapping("/board")
 @RequiredArgsConstructor
@@ -18,14 +22,15 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping({"", "/"})
-    public String board(@RequestParam(defaultValue = "0") Long idx, Model model) {
+    public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model) {
         model.addAttribute("board", boardService.findBoardByIdx(idx));
-        return "/board/form";
+        return "board/form";
     }
 
     @GetMapping("/list")
     public String list(@PageableDefault Pageable pageable, Model model) {
         model.addAttribute("boardList", boardService.findBoardList(pageable));
-        return "/board/list";
+        log.info("pageable={}", pageable);
+        return "board/list";
     }
 }
